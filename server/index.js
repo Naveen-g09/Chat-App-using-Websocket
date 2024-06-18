cce84a265e43851720c1330ba27bb57363d9df54
Naveen <naveengummella.rg@gmail.com>
2024-06-18 15:28:37 +0530

    some scripts and logs

--- server/index.js
+++ server/index.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const app = express();
 const PORT = 4000;
+const socketIO = require('socket.io')(http, {
+  cors: {
+    origin: "http://localhost:3000"
+  }
+});
 
-//New imports
 const http = require('http').Server(app);
 const cors = require('cors');
 
 app.use(cors());
+
+socketIO.on('connection', (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`);
+  socket.on('disconnect', () => {
+    console.log('🔥: A user disconnected');
+  });
+});
 
 app.get('/api', (req, res) => {
   res.json({
