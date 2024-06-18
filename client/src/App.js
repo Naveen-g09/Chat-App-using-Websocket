35a4ac0ad8f2425473411be4aa769326e7555a12
Naveen-g09 <naveengummella.rg@gmail.com>
2024-06-18 22:32:22 +0530

    complete app

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import ChatPage from './components/ChatPage';
-import socketIO from 'socket.io-client';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./components/Home"
+import ChatPage from "./components/ChatPage";
+import socketIO from "socket.io-client"
 
-const socket = socketIO.connect('http://localhost:4000');
+const socket = socketIO.connect("http://localhost:4000")
 function App() {
   return (
     <BrowserRouter>
@@ -14,6 +14,7 @@ function App() {
         </Routes>
       </div>
     </BrowserRouter>
+
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -1,37 +1,45 @@
-const express = require('express');
-const app = express();
-const PORT = 4000;
-
+const express = require("express")
+const app = express()
+const cors = require("cors")
 const http = require('http').Server(app);
-const cors = require('cors');
+const PORT = 4000
 const socketIO = require('socket.io')(http, {
-  cors: {
-    origin: "http://localhost:3000"
-  }
+    cors: {
+        origin: "http://localhost:3000"
+    }
 });
 
-
-app.use(cors());
+app.use(cors())
+let users = []
 
 socketIO.on('connection', (socket) => {
-  console.log(`⚡: ${socket.id} user just connected!`);
+    console.log(`⚡: ${socket.id} user just connected!`)  
+    socket.on("message", data => {
+      socketIO.emit("messageResponse", data)
+    })
 
-  socket.on('message', (data) => {
-    console.log(data);
-    socketIO.emit('messageResponse', data);
-  });
+    socket.on("typing", data => (
+      socket.broadcast.emit("typingResponse", data)
+    ))
 
-  socket.on('disconnect', () => {
-    console.log('🔥: A user disconnected');
-  });
+    socket.on("newUser", data => {
+      users.push(data)
+      socketIO.emit("newUserResponse", users)
+    })
+ 
+    socket.on('disconnect', () => {
+      console.log('🔥: A user disconnected');
+      users = users.filter(user => user.socketID !== socket.id)
+      socketIO.emit("newUserResponse", users)
+      socket.disconnect()
+    });
 });
 
-app.get('/api', (req, res) => {
-  res.json({
-    message: 'Hello world',
-  });
+app.get("/api", (req, res) => {
+  res.json({message: "Hello"})
 });
 
+   
 http.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+    console.log(`Server listening on ${PORT}`);
 });
